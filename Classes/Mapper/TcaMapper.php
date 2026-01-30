21d5ea4de4005dab27cd4cf9cfd97f63cac4860b
Stefan Galinski <stefan@sgalinski.de>
2026-01-30 20:43:39 +0100

    [FEATURE] TCAMapper - Allow to resolve child relations in a configurable depth

--- Classes/Mapper/TcaMapper.php
+++ Classes/Mapper/TcaMapper.php
@@ -27,8 +27,12 @@
 namespace SGalinski\SgApiCore\Mapper;
 
 use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Persistence\Generic\PersistenceManager;
+use TYPO3\CMS\Core\Resource\FileInterface;
 
 /**
  * Service for mapping TYPO3 records to API responses based on TCA and vice versa
@@ -45,12 +49,23 @@ class TcaMapper implements SingletonInterface {
 	protected ConnectionPool $connectionPool;
 
 	/**
+	 * @var ResourceFactory
+	 */
+	protected ResourceFactory $resourceFactory;
+
+	/**
 	 * @param PersistenceManager $persistenceManager
 	 * @param ConnectionPool $connectionPool
+	 * @param ResourceFactory $resourceFactory
 	 */
-	public function __construct(PersistenceManager $persistenceManager, ConnectionPool $connectionPool) {
+	public function __construct(
+		PersistenceManager $persistenceManager,
+		ConnectionPool $connectionPool,
+		ResourceFactory $resourceFactory
+	) {
 		$this->persistenceManager = $persistenceManager;
 		$this->connectionPool = $connectionPool;
+		$this->resourceFactory = $resourceFactory;
 	}
 
 	/**
@@ -59,6 +74,7 @@ class TcaMapper implements SingletonInterface {
 	 * @param object $object
 	 * @param string $tableName
 	 * @return array
+	 * @throws \Doctrine\DBAL\Exception
 	 */
 	public function getRawData(object $object, string $tableName): array {
 		$uid = (int) $this->persistenceManager->getIdentifierByObject($object);
@@ -81,13 +97,15 @@ class TcaMapper implements SingletonInterface {
 	 * @param array $record
 	 * @param array $allowedFields If empty, all fields except excluded ones are returned
 	 * @param array $excludedFields
+	 * @param int $resolveDepth Depth of relation resolution
 	 * @return array
 	 */
 	public function mapRecord(
 		string $tableName,
 		array $record,
 		array $allowedFields = [],
-		array $excludedFields = ['tstamp', 'crdate', 'cruser_id', 'hidden', 'deleted', 't3ver_oid', 't3ver_id', 't3ver_wsid', 't3ver_label', 't3ver_state', 't3ver_stage', 't3ver_count', 't3ver_tstamp', 't3ver_move_id', 't3_origuid', 'l10n_parent', 'l10n_diffsource', 'l10n_state']
+		array $excludedFields = ['tstamp', 'crdate', 'cruser_id', 'hidden', 'deleted', 't3ver_oid', 't3ver_id', 't3ver_wsid', 't3ver_label', 't3ver_state', 't3ver_stage', 't3ver_count', 't3ver_tstamp', 't3ver_move_id', 't3_origuid', 'l10n_parent', 'l10n_diffsource', 'l10n_state'],
+		int $resolveDepth = 0
 	): array {
 		$mappedRecord = [];
 		$tca = $GLOBALS['TCA'][$tableName] ?? [];
@@ -119,7 +137,7 @@ class TcaMapper implements SingletonInterface {
 			$value = $record[$fieldName];
 			$columnConfig = $tca['columns'][$fieldName]['config'] ?? [];
 
-			$mappedRecord[$fieldName] = $this->transformValue($value, $columnConfig);
+			$mappedRecord[$fieldName] = $this->transformValue($value, $columnConfig, $resolveDepth, $tableName, $record['uid'] ?? 0, $fieldName);
 		}
 
 		return $mappedRecord;
@@ -132,17 +150,19 @@ class TcaMapper implements SingletonInterface {
 	 * @param array $records
 	 * @param array $allowedFields
 	 * @param array $excludedFields
+	 * @param int $resolveDepth
 	 * @return array
 	 */
 	public function mapRecords(
 		string $tableName,
 		array $records,
 		array $allowedFields = [],
-		array $excludedFields = ['tstamp', 'crdate', 'cruser_id', 'hidden', 'deleted', 't3ver_oid', 't3ver_id', 't3ver_wsid', 't3ver_label', 't3ver_state', 't3ver_stage', 't3ver_count', 't3ver_tstamp', 't3ver_move_id', 't3_origuid', 'l10n_parent', 'l10n_diffsource', 'l10n_state']
+		array $excludedFields = ['tstamp', 'crdate', 'cruser_id', 'hidden', 'deleted', 't3ver_oid', 't3ver_id', 't3ver_wsid', 't3ver_label', 't3ver_state', 't3ver_stage', 't3ver_count', 't3ver_tstamp', 't3ver_move_id', 't3_origuid', 'l10n_parent', 'l10n_diffsource', 'l10n_state'],
+		int $resolveDepth = 0
 	): array {
 		$mappedRecords = [];
 		foreach ($records as $record) {
-			$mappedRecords[] = $this->mapRecord($tableName, $record, $allowedFields, $excludedFields);
+			$mappedRecords[] = $this->mapRecord($tableName, $record, $allowedFields, $excludedFields, $resolveDepth);
 		}
 		return $mappedRecords;
 	}
@@ -183,10 +203,32 @@ class TcaMapper implements SingletonInterface {
 	 *
 	 * @param mixed $value
 	 * @param array $config
+	 * @param int $resolveDepth
+	 * @param string $tableName
+	 * @param int $uid
+	 * @param string $fieldName
 	 * @return mixed
 	 */
-	protected function transformValue(mixed $value, array $config): mixed {
+	protected function transformValue(
+		mixed $value,
+		array $config,
+		int $resolveDepth = 0,
+		string $tableName = '',
+		int $uid = 0,
+		string $fieldName = ''
+	): mixed {
 		$type = $config['type'] ?? '';
+
+		if ($uid > 0 && $tableName !== '' && $fieldName !== '') {
+			$isFAL = ($type === 'inline' || $type === 'group') &&
+				(($config['foreign_table'] ?? '') === 'sys_file_reference' ||
+					($config['internal_type'] ?? '') === 'file_reference' ||
+					str_contains($config['MM'] ?? '', 'sys_file_reference'));
+
+			if ($isFAL) {
+				return $this->resolveFileReferences($tableName, $fieldName, $uid);
+			}
+		}
 
 		switch ($type) {
 			case 'check':
@@ -208,6 +250,38 @@ class TcaMapper implements SingletonInterface {
 				return str_contains($config['format'] ?? '', 'decimal') ? (float) $value : (int) $value;
 			case 'select':
 			case 'group':
+			case 'inline':
+				if ($resolveDepth > 0 && isset($config['foreign_table'])) {
+					$foreignTable = $config['foreign_table'];
+					if (is_string($value) && str_contains($value, ',')) {
+						$uids = GeneralUtility::intExplode(',', $value, TRUE);
+					} else {
+						$uids = [(int) $value];
+					}
+
+					$resolvedRecords = [];
+					foreach ($uids as $foreignUid) {
+						if ($foreignUid <= 0) {
+							continue;
+						}
+						$queryBuilder = $this->connectionPool->getQueryBuilderForTable($foreignTable);
+						$foreignRecord = $queryBuilder->select('*')
+							->from($foreignTable)
+							->where($queryBuilder->expr()->eq('uid', $queryBuilder->createNamedParameter($foreignUid)))
+							->executeQuery()
+							->fetchAssociative();
+
+						if ($foreignRecord) {
+							$resolvedRecords[] = $this->mapRecord($foreignTable, $foreignRecord, [], [], $resolveDepth - 1);
+						}
+					}
+
+					if (isset($config['maxitems']) && (int) $config['maxitems'] <= 1 && count($resolvedRecords) <= 1) {
+						return $resolvedRecords[0] ?? NULL;
+					}
+					return $resolvedRecords;
+				}
+
 				if (isset($config['maxitems']) && (int) $config['maxitems'] > 1) {
 					return is_string($value) ? explode(',', $value) : $value;
 				}
@@ -215,6 +289,40 @@ class TcaMapper implements SingletonInterface {
 			default:
 				return $value;
 		}
+	}
+
+	/**
+	 * Resolves FAL file references for a given field
+	 *
+	 * @param string $tableName
+	 * @param string $fieldName
+	 * @param int $uid
+	 * @return array
+	 */
+	protected function resolveFileReferences(string $tableName, string $fieldName, int $uid): array {
+		$fileReferences = $this->resourceFactory->getFileReferenceObjectsByElement(
+			$uid,
+			$tableName,
+			$fieldName
+		);
+
+		$resolvedFiles = [];
+		foreach ($fileReferences as $fileReference) {
+			/** @var FileReference $fileReference */
+			$file = $fileReference->getOriginalFile();
+			$resolvedFiles[] = [
+				'uid' => $fileReference->getUid(),
+				'title' => $fileReference->getTitle() ?: ($file->getProperty('title') ?: $file->getName()),
+				'description' => $fileReference->getDescription() ?: $file->getProperty('description'),
+				'alternative' => $fileReference->getAlternative(),
+				'url' => $file->getPublicUrl(),
+				'extension' => $file->getExtension(),
+				'size' => $file->getSize(),
+				'mime_type' => $file->getMimeType(),
+			];
+		}
+
+		return $resolvedFiles;
 	}
 
 	/**
