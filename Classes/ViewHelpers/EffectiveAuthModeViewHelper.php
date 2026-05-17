8d37ea587af20b8b88db9b3ed29aaba5c58c80fc
Stefan Galinski <stefan@sgalinski.de>
2026-05-17 18:47:42 +0200

    [TASK] New Copyright Header

--- Classes/ViewHelpers/EffectiveAuthModeViewHelper.php
+++ Classes/ViewHelpers/EffectiveAuthModeViewHelper.php
@@ -1,5 +1,17 @@
 <?php
 
+/***************************************************************
+ *  Copyright notice
+ *  (c) sgalinski Internet Services (https://www.sgalinski.de)
+ *
+ *  All rights reserved
+ *
+ *  This file is part of the TYPO3 CMS project.
+ *  It is free software; you can redistribute it and/or modify it under
+ *  the terms of the "GNU General Public License", either version 3
+ *  of the License or any later version.
+ ***************************************************************/
+
 namespace SGalinski\SgApiCore\ViewHelpers;
 
 use SGalinski\SgApiCore\Service\ApiRegistry;
